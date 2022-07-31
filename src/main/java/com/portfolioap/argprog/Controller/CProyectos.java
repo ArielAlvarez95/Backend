091f76a384ba27b0ff1cc1db2b2aa7b14bd4f485
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-07-31 04:04:30 -0300

    por hacer el deploy

--- src/main/java/com/portfolioap/argprog/Controller/CProyectos.java
+++ src/main/java/com/portfolioap/argprog/Controller/CProyectos.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("pro")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://proyectofinalargprog-24f4c.web.app/")
 public class CProyectos {
 
     @Autowired
