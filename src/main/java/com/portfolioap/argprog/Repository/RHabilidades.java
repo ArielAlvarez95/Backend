f189ddab90f89a9f3cc1bf65faf7312ab68abd4c
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-07-30 21:22:12 -0300

    notation repository

--- src/main/java/com/portfolioap/argprog/Repository/RHabilidades.java
+++ src/main/java/com/portfolioap/argprog/Repository/RHabilidades.java
@@ -3,8 +3,9 @@ package com.portfolioap.argprog.Repository;
 import com.portfolioap.argprog.Entity.Habilidades;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface RHabilidades extends JpaRepository<Habilidades, Integer> {
     public Optional <Habilidades> findByNombreH(String nombreH);
     public boolean existsByNombreH(String nombreH);
