5eee932754cbe6b09dd96563aa293e27d24f924e
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-08-02 18:31:08 -0300

    make it better

--- src/main/java/com/portfolioap/argprog/Controller/CExperiencia.java
+++ src/main/java/com/portfolioap/argprog/Controller/CExperiencia.java
@@ -21,8 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/explab")
-@CrossOrigin(origins = "https://proyectofinalargprog.herokuapp.com/")
+@RequestMapping("/explab/")
+@CrossOrigin(origins = "https://proyectofinalargprog.herokuapp.com/explab/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
