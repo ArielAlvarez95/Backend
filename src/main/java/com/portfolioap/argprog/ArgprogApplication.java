2a487741da3b1b8b5ee92cd0feaeb6fb5ff2f58e
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-08-02 03:06:56 -0300

    make it better

--- src/main/java/com/portfolioap/argprog/ArgprogApplication.java
+++ src/main/java/com/portfolioap/argprog/ArgprogApplication.java
@@ -19,7 +19,7 @@ public class ArgprogApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://proyectofinalargprog.herokuapp.com/")
+                        .allowedOrigins("/**")
                         .allowedHeaders("*")
                         .allowedMethods("*");
             }
