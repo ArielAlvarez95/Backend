bb9340d7ef884fae3e0f6447cef88454730a16b1
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-08-02 03:24:51 -0300

    make it better

--- src/main/java/com/portfolioap/argprog/ArgprogApplication.java
+++ src/main/java/com/portfolioap/argprog/ArgprogApplication.java
@@ -1,31 +1,11 @@
 package com.portfolioap.argprog;
 
-import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 @SpringBootApplication
 public class ArgprogApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ArgprogApplication.class, args);
     }
-
-@Bean
-    CorsConfigurationSource corsConfigurationSource() {
-     CorsConfiguration cc = new CorsConfiguration();
-                cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
-                cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                
-                cc.setAllowedOrigins(Arrays.asList("/*"));
-                cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
-                cc.addAllowedOrigin("*");
-                cc.setAllowCredentials(Boolean.TRUE);
-  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-  source.registerCorsConfiguration("/**", cc);
-  return source;
- }
  }
--- src/main/java/com/portfolioap/argprog/security/MainSecurity.java
+++ src/main/java/com/portfolioap/argprog/security/MainSecurity.java
@@ -3,6 +3,7 @@ package com.portfolioap.argprog.security;
 import com.portfolioap.argprog.security.Service.UserDetailsImpl;
 import com.portfolioap.argprog.security.jwt.JwtEntryPoint;
 import com.portfolioap.argprog.security.jwt.JwtTokenFilter;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -64,4 +68,18 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsServicesImpl).passwordEncoder(passwordEncoder());
     }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+     CorsConfiguration cc = new CorsConfiguration();
+                cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
+                cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                
+                cc.setAllowedOrigins(Arrays.asList("/*"));
+                cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
+                cc.addAllowedOrigin("*");
+                cc.setAllowCredentials(Boolean.TRUE);
+  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  source.registerCorsConfiguration("/**", cc);
+  return source;
+ }
 }
