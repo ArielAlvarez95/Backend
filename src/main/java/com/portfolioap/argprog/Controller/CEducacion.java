7f91e121f0a4e8bc5e09dc21b6adbe564073c63d
ArielAlvarez95 <alvarezarielb@gmail.com>
2022-08-02 19:04:33 -0300

    make it better

--- src/main/java/com/portfolioap/argprog/Controller/CEducacion.java
+++ src/main/java/com/portfolioap/argprog/Controller/CEducacion.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/educ")
+@RequestMapping("/educ/")
 @CrossOrigin(origins = "https://proyectofinalargprog-24f4c.web.app/educ")
 public class CEducacion {
     @Autowired
